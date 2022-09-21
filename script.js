31c4ea9a63dffb480005e99919275f2cb9752b57
matoriart <aromofficial158@gmail.com>
2022-09-21 14:00:39 +0700

    alpha 1.2

---
+++ script.js
@@ -0,0 +1,14 @@
+//scroll
+const sr = ScrollReveal ({
+    distance: '10px',
+    duration: 2100,
+    reset: true
+})
+
+sr.reveal('.asx',{delay:200, origin:'left'}),
+sr.reveal('.container,.anime-news,#box1 img,#box2 img,#box3 img,#Wallpaper h1,#carouselExampleIndicators',{delay:100, origin:'bottom'})
+//popup
+function popupToggle(){
+    const popup = document.getElementById("popup");
+}
+//blur
